1fdbf83dcc9fe66c1395894a2d8cdf211428110b
Kunj Arora <kunj57932@gmail.com>
2022-06-15 23:26:51 +0530

    added basic API routes and error custom  handler

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB= async()=>{
+    try{
+        const conn=await mongoose.connect(process.env.MONGO_URI,{
+            useUnifiedTopology:true,
+            useNewUrlParser:true
+        })
+        console.log(`DB connected: ${conn.connection.host}`.cyan.underline)
+    } catch(err){
+        console.log("error ".red.underline.bold,err.message)
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ backend/server.js
@@ -0,0 +1,35 @@
+// const express = require('express')
+// const ck = require('ckey');
+import ck from 'ckey'
+import express from 'express'
+// import products from './data/products.js'
+import connectDB from './config/db.js'
+import colors from 'colors'
+const app = express()
+import productRoutes from './routes/productRoutes.js'
+import {notFound,errorHandler} from './middleware/errorMiddleware.js'
+
+connectDB()
+
+
+
+// const products=require('./data/products')
+
+// console.log(process.env)
+
+app.get('/', (req, res) => {
+    res.send('api running')
+})
+
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`server running in  mode ${process.env.NODE_ENV} on ${PORT}`)
+})
+

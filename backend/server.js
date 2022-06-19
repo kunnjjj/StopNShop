1455cf3bebd2f46fe359d89100760948f878d46a
Kunj Arora <kunj57932@gmail.com>
2022-06-19 15:59:04 +0530

    JWT authorization , login api

--- backend/server.js
+++ backend/server.js
@@ -5,23 +5,23 @@ import express from 'express'
 // import products from './data/products.js'
 import connectDB from './config/db.js'
 import colors from 'colors'
-const app = express()
+
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import {notFound,errorHandler} from './middleware/errorMiddleware.js'
 
+const app = express()
 connectDB()
 
+app.use(express.json())
 
-
-// const products=require('./data/products')
-
-// console.log(process.env)
-///
 app.get('/', (req, res) => {
     res.send('api running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+
 
 app.use(notFound)
 app.use(errorHandler)

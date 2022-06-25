27622e9be5afe61521a3f349a1785ff65b4a7745
Kunj Arora <kunj57932@gmail.com>
2022-06-25 22:34:36 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,7 @@ app.use(express.json())
 
 
 
-app.get('/', (req, res) => {
-    res.send('api running')
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -39,8 +37,19 @@ app.get('/api/config/paypal', (req, res) => {
     res.send(process.env.PAYPAL_CLIENT_ID)
 })
 
-
 const __dirname = path.resolve()
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')))
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    })
+} else{
+    app.get('/', (req, res) => {
+        res.send('api running')
+    })
+}
+
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
